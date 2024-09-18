7f498f1c61e5d40d7fdeafe98b5b4047a3e052b6
Tom Schmidt <wocif@protonmail.com>
2024-09-18 17:18:41 +0200

    added Player + Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,35 +6,87 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1f;
+    public Sprite[] walkSprites; // Array von Lauf-Sprites
+    [SerializeField] private float animationSpeed = 0.1f;
+    public Sprite staySprite; // Stand-Sprite
 
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private int currentSpriteIndex = 0;
+    private float timeSinceLastFrame = 0f;
+
+    private Animator spriteAnimator;
+
 
     private void Awake()
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteAnimator = GetComponent<Animator>();
     }
 
     private void OnEnable()
     {
         playerControls.Enable();
+        playerControls.Movement.Move.performed += OnMove;
+        playerControls.Movement.Move.canceled += OnMove;
     }
-    
-    private void PlayerInput()
+
+    private void OnDisable()
     {
-        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+        playerControls.Disable();
+        playerControls.Movement.Move.performed -= OnMove;
+        playerControls.Movement.Move.canceled -= OnMove;
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+        bool isMoving = movement != Vector2.zero;
+        spriteAnimator.SetBool("IsMoving", isMoving); // Animator-Parameter setzen
     }
 
     private void Move()
     {
-        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+        rb.velocity = movement * moveSpeed; // Geschwindigkeit basierend auf Eingabe setzen
     }
+
+    private void AnimateMovement()
+    {
+        timeSinceLastFrame += Time.deltaTime;
+        if (timeSinceLastFrame >= animationSpeed)
+        {
+            if (walkSprites.Length > 0)
+            {
+                currentSpriteIndex = (currentSpriteIndex + 1) % walkSprites.Length;
+                spriteRenderer.sprite = walkSprites[currentSpriteIndex];
+                timeSinceLastFrame = 0f;
+            }
+            else
+            {
+                Debug.LogError("walkSprites array is empty!");
+            }
+        }
+    }
+
     private void Update()
     {
-        PlayerInput();
+        bool isMoving = movement != Vector2.zero;
+        Debug.Log($"Movement: {movement}, IsMoving: {isMoving}"); // Debug-Ausgabe
+
+        if (isMoving)
+        {
+            AnimateMovement();
+        }
+        else
+        {
+            spriteRenderer.sprite = staySprite; // Zeigt das Stand-Sprite an
+        }
     }
+
 
     private void FixedUpdate()
     {
