f87188c9741f07412eb3944ab87d1193bcb5816c
phuongias <phng2912@gmail.com>
2024-09-20 12:29:01 +0200

    add panel + infoMessage

--- Assets/Scripts/RocketTeilController.cs
+++ Assets/Scripts/RocketTeilController.cs
@@ -7,19 +7,25 @@ using UnityEngine.UI;
 public class RocketTeilController : MonoBehaviour
 
 {
-    [SerializeField]
-    public TextMeshProUGUI progressText; 
-    public static int totalParts = 3; 
-    public static int partsPickedUp = 0; 
+    [SerializeField] public TextMeshProUGUI progressText;
+    public static int totalParts = 3;
+    public static int partsPickedUp = 0;
 
-    [SerializeField]
-    private TextMeshProUGUI pickUpText;
+    [SerializeField] private TextMeshProUGUI pickUpText;
     private bool pickUpAllowed;
+
+    [SerializeField] private GameObject messagePanel;
+    [SerializeField] private TextMeshProUGUI messageText;
+    private List<string> infoMessages;
 
     private void Start()
     {
         pickUpText.gameObject.SetActive(false);
+        messageText.gameObject.SetActive(false);
+        messagePanel.SetActive(false);
+
         UpdateProgressText();
+        InitializeMessages();
     }
 
     private void Update()
@@ -48,11 +54,12 @@ public class RocketTeilController : MonoBehaviour
 
     private void PickUp()
     {
-        partsPickedUp++; // Erhöhe die Anzahl der gesammelten Teile
+        partsPickedUp++;
         UpdateProgressText();
+        ShowInfoMessage(partsPickedUp - 1);
         Destroy(gameObject);
     }
-    
+
     private void UpdateProgressText()
     {
         if (progressText != null)
@@ -63,5 +70,33 @@ public class RocketTeilController : MonoBehaviour
                 progressText.text = $"Alle {totalParts} Raketenteile eingesammelt.";
             }
         }
+    }
+
+    private void InitializeMessages()
+    {
+        infoMessages = new List<string>
+        {
+            "Gut gemacht! Wusstest du, dass der Mond keine Atmosphäre hat?",
+            "Klasse! Hättest du gedacht, dass ......",
+            "Super! Vielleicht sind wir nicht die einzigen Lebenden im Universum...!"
+        };
+    }
+
+    private void ShowInfoMessage(int messageIndex)
+    {
+        if (messageIndex >= 0 && messageIndex < infoMessages.Count)
+        {
+            messageText.text = infoMessages[messageIndex];
+            messagePanel.SetActive(true);
+            messageText.gameObject.SetActive(true);
+            StartCoroutine(HideMessageAfterKeyPress(5f));
+        }
+    }
+
+    private IEnumerator HideMessageAfterKeyPress(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        messagePanel.SetActive(false);
+        messageText.gameObject.SetActive(false);
     }
 }
